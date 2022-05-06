383690fd9a4491f5e6e84a43b93335c402077166
Sancatau <rouale@mail.ru>
2022-05-06 20:11:12 +0300

    5
    5 Lection

--- Assets/Scripts/CanvasButtons.cs
+++ Assets/Scripts/CanvasButtons.cs
@@ -7,4 +7,9 @@ public class CanvasButtons : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void LoadItch()
+    {
+        Application.OpenURL("https://itch.io");
+    }
 }
